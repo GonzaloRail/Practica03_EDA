0c5895e0067afde5b1ca3e1141d9725a11e4116f
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-06-17 10:08:29 -0500

    cambiando el tostring

--- Persona.java
+++ Persona.java
@@ -40,10 +40,6 @@ public class Persona implements Comparable<Persona> {
 
     @Override
     public String toString() {
-        return "Persona{" +
-                "nombre='" + nombre + '\'' +
-                ", apellido='" + apellido + '\'' +
-                ", edad=" + edad +
-                '}';
+        return "nombre: " + nombre + "\t\t apellido: " + apellido +"\t\t edad: " + edad;
     }
 }
